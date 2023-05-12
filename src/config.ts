718ee5709bf25d06ab71619158fcc52e81c43a82
“Miray <miray.kaptan@westerops.com>
2023-05-12 11:43:14 +0300

    Firebase config added.

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfiguration = {
+  apiKey: "AIzaSyDZu9kbtBX-y9ortRNgVsMNJBJ7UzGc-RY",
+  authDomain: "login-page-ed9d8.firebaseapp.com",
+  projectId: "login-page-ed9d8",
+  storageBucket: "login-page-ed9d8.appspot.com",
+  messagingSenderId: "451061013683",
+  appId: "1:451061013683:web:fe56a38c0e3a6131e364c6",
+};
