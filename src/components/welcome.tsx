230808d78b99981ca1a40796f7ff388582ffb504
“Miray <miray.kaptan@westerops.com>
2023-05-22 13:21:28 +0300

    login w/openId button added

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -6,14 +6,21 @@ interface WelcomeProps {}
 const Welcome: FunctionComponent<WelcomeProps> = () => {
   const navigate = useNavigate();
   return (
-    <div className="p-20 justify-center items-center">
-      <p>Welcome to the page!</p>
+    <div className="p-20 justify-center items-center ">
+      <p className="mb-5 flex justify-center">Welcome to the page!</p>
       <button
-        className="w-full h-10 text-white bg-blue-700 rounded-lg"
+        className="w-full h-10 text-white bg-orange-400 rounded-lg mb-10"
         type="button"
         onClick={() => navigate("./login-page")}
       >
-        Click here to log-in!
+        Click here to log-in with Firebase!
+      </button>
+      <button
+        className="w-full h-10 text-white bg-blue-700 rounded-lg"
+        type="button"
+        // onClick={() => navigate("./login-page")}
+      >
+        Click here to log-in with OpenId!
       </button>
     </div>
   );
