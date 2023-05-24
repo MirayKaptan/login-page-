2d81521de65d61bbf5ad56a53cf1373857a7ce4c
“Miray <miray.kaptan@westerops.com>
2023-05-25 01:04:56 +0300

    dashboard page created to control user logged in or not

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/welcome";
 import LoginPage from "./pages/loginPage";
 import RegistrationPage from "./pages/registrationPage";
+import Dashboard from "./pages/dashboard";
 
 export interface AppProps {}
 
@@ -14,6 +15,7 @@ const App: FunctionComponent<AppProps> = () => {
           <Route path="/" element={<WelcomePage />} />
           <Route path="/login-page" element={<LoginPage />} />
           <Route path="/registration-page" element={<RegistrationPage />} />
+          <Route path="/dashboard-page" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface DashboardProps {}
+
+const Dashboard: FunctionComponent<DashboardProps> = () => {
+  return <div>You logged in.</div>;
+};
+
+export default Dashboard;
