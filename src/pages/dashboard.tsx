3cbd19cfe7af33daa8dd64558d0a7812c3b51cc9
“Miray <miray.kaptan@westerops.com>
2023-05-26 00:47:05 +0300

    Logout from dashboard added.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,29 @@
+import { signOut } from "firebase/auth";
 import { FunctionComponent } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../config/config";
 
 interface DashboardProps {}
 
 const Dashboard: FunctionComponent<DashboardProps> = () => {
-  return <div>You logged in.</div>;
+  const navigate = useNavigate();
+  const handleLogout = (event: React.FormEvent) => {
+    event.preventDefault();
+    signOut(auth).then(() => {
+      navigate("/");
+    });
+  };
+  return (
+    <div className="flex flex-col items-center gap-5 p-20">
+      You logged in.
+      <button
+        className="w-full h-10 text-white bg-blue-700 rounded-lg"
+        onClick={handleLogout}
+      >
+        Log out
+      </button>
+    </div>
+  );
 };
 
 export default Dashboard;
